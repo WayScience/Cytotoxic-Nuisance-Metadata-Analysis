c6ece82e3bf6914301cfef835fe12024646bbb10
axiomcura <erikishere3@gmail.com>
2024-03-11 12:39:08 -0600

    added nbconveted

--- notebooks/0.freature_selection/nbconverted/feature_processing.py
+++ notebooks/0.freature_selection/nbconverted/feature_processing.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
+# # Feature Processing and Selection
+# This notebook focuses on exploration using two essential files: the annotations data extracted from the actual screening profile (available in the [IDR repository](https://github.com/IDR/idr0133-dahlin-cellpainting/tree/main/screenA)) and the metadata retrieved from the supplementary section of the [research paper](https://static-content.springer.com/esm/art%3A10.1038%2Fs41467-023-36829-x/MediaObjects/41467_2023_36829_MOESM5_ESM.xlsx).
+#
+# We explore the number of unique compounds associated with each cell injury and subsequently cross-reference this information with the screening profile. The aim is to assess the feasibility of using the data for training a machine learning model to predict cell injury.
+#
+# We apply feature selection through [pycytominer](https://github.com/cytomining/pycytominer) to capture the most informative features representing various cellular injury types within the morphology space. Then, we utilize the selected feature profiles for machine learning applications.
+#
+
+# In[1]:
 
 
 import pathlib
@@ -14,7 +22,7 @@ from pycytominer import feature_select
 sys.path.append("../../")
 from src import utils
 
-# In[9]:
+# In[2]:
 
 
 # data directory
@@ -36,7 +44,19 @@ suppl_meta_df = pd.read_csv(suppl_meta_path)
 cell_injury_df = suppl_meta_df[["Cellular injury category", "Compound alias"]]
 
 
-# In[10]:
+# In[3]:
+
+
+# get the control
+control_df = image_profile_df.loc[image_profile_df["Compound Name"] == "DMSO"]
+control_df.insert(0, "injury_type", "Control")
+
+# display
+print("Shape of the control:", control_df.shape)
+control_df.head()
+
+
+# In[4]:
 
 
 # getting profiles based on injury and compound type
@@ -54,7 +74,7 @@ for injury_type, compound_list in injury_and_compounds.items():
     injury_profiles.append(sel_profile)
 
 
-# In[11]:
+# In[5]:
 
 
 # creating a dataframe that contains stratified screen Data
@@ -69,7 +89,7 @@ print("shape:", injured_df.shape)
 injured_df.head()
 
 
-# In[12]:
+# In[6]:
 
 
 # seperating meta and feature columns
@@ -77,7 +97,7 @@ meta = injured_df.columns.tolist()[:32]
 features = injured_df.columns.tolist()[32:]
 
 
-# In[13]:
+# In[7]:
 
 
 # dropping samples that have at least 1 NaN
@@ -88,7 +108,7 @@ print("Shape after removing samples: ", injured_df.shape)
 injured_df.head()
 
 
-# In[15]:
+# In[8]:
 
 
 # setting feature selection operations
@@ -112,25 +132,46 @@ fs_injury_df = feature_select(
     outlier_cutoff=100,
 )
 
-# saving dataframe
-fs_injury_df.to_csv(
-    fs_dir / "cell_injury_profile_fs.csv.gz",
-    index=False,
-    compression="gzip",
-)
 
-
-# In[16]:
+# In[9]:
 
 
 print("Feature selected profile shape:", fs_injury_df.shape)
 fs_injury_df.head()
 
 
-# In[20]:
+# In[10]:
+
+
+# update the control with the retained features in the injury_fs_profile
+control_df = control_df[fs_injury_df.columns]
+
+# display
+print(
+    "Shape of control after using feature retained from injury_fs profile",
+    control_df.shape,
+)
+control_df.head()
+
+
+# In[11]:
+
+
+# concat both the injury and control together and make this is that feature selected profile
+fs_profile = pd.concat([control_df, fs_injury_df])
+
+# save and display
+fs_profile.to_csv(
+    fs_dir / "cell_injury_profile_fs.csv.gz",
+    index=False,
+    compression="gzip",
+)
+
+
+# In[12]:
 
 
 # setting which injr
-cell_injuries = fs_injury_df["injury_type"].unique()
+cell_injuries = fs_profile["injury_type"].unique()
 print("number of cell injury types", len(cell_injuries))
 cell_injuries
--- notebooks/1.data_splits/nbconverted/data_splits.py
+++ notebooks/1.data_splits/nbconverted/data_splits.py
@@ -2,13 +2,7 @@
 # coding: utf-8
 
 # # Spliting Data
-#
-# This notebook focuses on exploration using two essential files: the annotations data extracted from the actual screening profile (available in the [IDR repository](https://github.com/IDR/idr0133-dahlin-cellpainting/tree/main/screenA)) and the metadata retrieved from the supplementary section of the [research paper](https://static-content.springer.com/esm/art%3A10.1038%2Fs41467-023-36829-x/MediaObjects/41467_2023_36829_MOESM5_ESM.xlsx).
-#
-# We explore the number of unique compounds associated with each cell injury and subsequently cross-reference this information with the screening profile. The aim is to assess the feasibility of using the data for training a machine learning model to predict cell injury.
-#
-# Next, with the information collected, we then split our data, identify the controls, and determine our training, test, and holdout sets for subsequent analysis.
-#
+# Here, we utilize the feature-selected profiles generated in the preceding module notebook [here](../0.freature_selection/), focusing on dividing the data into training, testing, and holdout sets for machine learning training.
 
 # In[1]:
 
@@ -16,12 +10,9 @@
 import json
 import pathlib
 import warnings
-from collections import defaultdict
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 
 # ignoring warnings
@@ -37,113 +28,71 @@ warnings.catch_warnings(action="ignore")
 # In[2]:
 
 
-# data directory
+# directory to get all the inputs for this notebook
 data_dir = pathlib.Path("../../data").resolve(strict=True)
 results_dir = pathlib.Path("../../results").resolve(strict=True)
-data_split_dir = (results_dir / "0.data_splits").resolve()
+fs_dir = (results_dir / "0.feature_selection").resolve(strict=True)
+
+# directory to store all the output of this notebook
+
+data_split_dir = (results_dir / "1.data_splits").resolve()
 data_split_dir.mkdir(exist_ok=True)
 
-# data paths
-suppl_meta_path = data_dir / "41467_2023_36829_MOESM5_ESM.csv.gz"
-screen_anno_path = data_dir / "idr0133-screenA-annotation.csv.gz"
-
-# load data
-image_profile_df = pd.read_csv(screen_anno_path)
-meta_df = image_profile_df[image_profile_df.columns[:31]]
-compounds_df = meta_df[["Compound Name", "Compound Class"]]
-
-suppl_meta_df = pd.read_csv(suppl_meta_path)
-cell_injury_df = suppl_meta_df[["Cellular injury category", "Compound alias"]]
-
-
-# ## Data splitting and exploration
-
-# In this notebook, we explore the structure of the experiments, focusing on how treatments were applied, conducting quality data checks, and identifying other relationships within the dataset.
-#
-# Additionally, we extract datasets that will be utilized in our machine learning processes. These datasets encompass our training data, control sets, and holdout sets.
-
-# -----
-
-# The code below, we extract information regarding various injury types and the corresponding number of compounds known to induce each type of injury.
-# then, we cross-reference with the selected compounds and identify wells that have a match in the profile data.
-#
-# This will be our training dataset, where it contains all the cellular injury labeles and it will be used for machine learning modeling
 
 # In[3]:
 
 
-# getting profilies based on injury and compound type
-injury_and_compounds = defaultdict(list)
-for injury, compound in cell_injury_df.values.tolist():
-    injury_and_compounds[injury].append(compound)
+# data paths
+fs_profile_path = (fs_dir / "cell_injury_profile_fs.csv.gz").resolve(strict=True)
 
-# cross reference selected injury and associated components into the screen profile
-injury_profiles = []
-for injury_type, compound_list in injury_and_compounds.items():
-    sel_profile = image_profile_df[
-        image_profile_df["Compound Name"].isin(compound_list)
-    ]
-    sel_profile.insert(0, "injury_type", injury_type)
-    injury_profiles.append(sel_profile)
+# load data
+fs_profile_df = pd.read_csv(fs_profile_path)
 
+# display
+print("fs profile with control: ", fs_profile_df.shape)
+fs_profile_df.head()
+
+
+# ## Exploring the data set
+#
+# Below is a  exploration of the selected features dataset. The aim is to identify treatments, extract metadata, and gain a understanding of the experiment's design.
+
+# Below demonstrates the amount of wells does each treatment have.
 
 # In[4]:
 
 
-# creating a dataframe that contains stratified screen Data
-injured_df = pd.concat(injury_profiles)
+# displying the amount of wells per treatments
+well_treatments_counts_df = (
+    fs_profile_df["Compound Name"].value_counts().to_frame().reset_index()
+)
+well_treatments_counts_df
 
-# display df
-print(injured_df.shape)
-injured_df.head()
 
-
-# >This DataFrame categorizes wells based on their injury types and with its corresponding compounds linked to each specific injury type.
-# > Note the new column `injury_type` indicating the assigned injury type for each well.
-# > This assignment is determined by the component with which the well has been treated.
+# Below we show the amount of wells does a specific cell celluar injury has
 
 # In[5]:
 
 
-neg_control_df = image_profile_df.loc[image_profile_df["Compound Name"] == "DMSO"]
-neg_control_df.insert(0, "injury_type", "Control")
-neg_control_df.to_csv("negative_control.csv.gz", compression="gzip", index=False)
-
-print("Shape:", neg_control_df.shape)
-neg_control_df.head()
-
-
-# > This dataframe contains all the wells that are considered to be controls.
-# > All of these wells have been treated with DMSO
-
-# Now that our dataset is labeled, the next step is to concatenate them and begin splitting the dataset into training, testing, and holdout sets. Additionally, we'll extract metadata from the labeled data to gain a better understanding of how the treatments and plates were generated in this experiment.
-
-# In[6]:
-
-
-# creating labeled_df
-labeled_df = pd.concat([neg_control_df, injured_df]).reset_index(drop=True)
-
-# only select entries where there are not NaN in the 'injury_type'
-labeled_df = labeled_df.loc[~labeled_df["injury_type"].isna()]
-
-# save and display
-print("shape:", labeled_df.shape)
-labeled_df.head()
+# Displaying how many how wells does each cell injury have
+cell_injury_well_counts = (
+    fs_profile_df["injury_type"].value_counts().to_frame().reset_index()
+)
+cell_injury_well_counts
 
 
 # Here, we're storing the metadata and feature column names into a JSON file to simplify loading during feature engineering processes.
 #
 # This will be saved in the `results/0.data_splits` directory
 
-# In[7]:
+# In[6]:
 
 
 # collecting metadata and feature column names
-feature_cols = labeled_df.columns[32:].tolist()
+feature_cols = fs_profile_df.columns[32:].tolist()
 raw_features = {
     "compartments": list(set([name.split("_")[0] for name in feature_cols])),
-    "meta_features": injured_df.columns[:32].tolist(),
+    "meta_features": fs_profile_df.columns[:32].tolist(),
     "feature_cols": feature_cols,
 }
 
@@ -156,11 +105,11 @@ with open(data_split_dir / "raw_feature_names.json", mode="w") as stream:
 #
 # This will be saved in the `results/0.data_splits` directory
 
-# In[8]:
+# In[7]:
 
 
 meta_injury = []
-for injury_type, df in labeled_df.groupby("injury_type"):
+for injury_type, df in fs_profile_df.groupby("injury_type"):
     # extract n_wells, n_compounds and unique compounds per injury_type
     n_wells = df.shape[0]
     unique_compounds = list(df["Compound Name"].unique())
@@ -179,42 +128,17 @@ print("shape:", injury_meta_df.shape)
 injury_meta_df
 
 
-# > This DataFrame contains information about wells associated with a specific injury type.
-# > It includes details such as the number of components used along with the list of the components responsible for the identified injury type.
-
-#
-
-# In[9]:
-
-
-# make a figure of this table
-plt.figure(figsize=(12, 8))
-
-# plotting
-sns.barplot(
-    data=injury_meta_df,
-    x="n_wells",
-    y="injury_type",
-    palette="viridis",
-)
-plt.xlabel("Numer of Wells")
-plt.ylabel("Injury Type")
-plt.title("Number of Wells per Injury Type")
-
-plt.show()
-
-
 # > Barchart showing the number of wells that are labeled with a given injury
 
 # Next, we construct the profile metadata. This provides a structured overview of how the treatments assicoated with injuries were applied, detailing the treatments administered to each plate.
 #
 # This will be saved in the `results/0.data_splits` directory
 
-# In[10]:
+# In[8]:
 
 
 injury_meta_dict = {}
-for injury, df in labeled_df.groupby("injury_type"):
+for injury, df in fs_profile_df.groupby("injury_type"):
     # collecting treatment metadata
     plates = df["Plate"].unique().tolist()
     treatment_meta = {}
@@ -244,11 +168,11 @@ with open(data_split_dir / "injury_metadata.json", mode="w") as stream:
 #
 # This will be saved in `results/0.data_splits`
 
-# In[11]:
+# In[9]:
 
 
 plate_meta = {}
-for plate_id, df in labeled_df.groupby("Plate"):
+for plate_id, df in fs_profile_df.groupby("Plate"):
     unique_compounds = list(df["Compound Name"].unique())
     n_treatments = len(unique_compounds)
 
@@ -284,7 +208,7 @@ with open(data_split_dir / "plate_info.json", mode="w") as stream:
 #
 # Here we are randomly select 15 wells from each treatment from the main dataframe `labeled_df`. Then we remove those wells from the main dataframe
 
-# In[12]:
+# In[10]:
 
 
 #### Plate heldout dataset
@@ -293,7 +217,7 @@ n_samples = 15
 
 # collecting randomly select wells based on treatment
 treatment_holdout_df = []
-for treatment, df in labeled_df.groupby("Compound Name", as_index=False):
+for treatment, df in fs_profile_df.groupby("Compound Name", as_index=False):
     heldout_treatment = df.sample(n=15, random_state=seed)
     treatment_holdout_df.append(heldout_treatment)
 
@@ -303,10 +227,10 @@ treatment_holdout_df = pd.concat(treatment_holdout_df)
 # take the indices of the held out data frame and use it to drop those samples from
 # the main dataset. And then check if those indices are dropped
 treatment_idx_to_drop = treatment_holdout_df.index.tolist()
-labeled_df = labeled_df.drop(treatment_idx_to_drop)
+fs_profile_df = fs_profile_df.drop(treatment_idx_to_drop)
 assert all(
     [
-        True if num not in labeled_df.index.tolist() else False
+        True if num not in fs_profile_df.index.tolist() else False
         for num in treatment_idx_to_drop
     ]
 ), "index to be dropped found in the main dataframe"
@@ -327,7 +251,7 @@ treatment_holdout_df.head()
 #
 # To generate the well hold out data, each plate was iterated and random wells were selected. However, an additional step was condcuting which was to seperate the control wells and the treated wells, due to the large label imbalance with the controls. Therefore, 5 wells were randomly selected and 10 wells were randomly selected from each individual plate
 
-# In[13]:
+# In[11]:
 
 
 # parameters
@@ -341,8 +265,8 @@ np.random.seed(seed)
 
 # collecting randomly select wells based on treatment
 wells_heldout_df = []
-for treatment, df in labeled_df.groupby("Plate", as_index=False):
-    # seperate control wells and rest of all wells since there is a huge label imbalance
+for treatment, df in fs_profile_df.groupby("Plate", as_index=False):
+    # separate control wells and rest of all wells since there is a huge label imbalance
     # selected 5 control wells and 10 random wells from the plate
     df_control = df.loc[df["Compound Name"] == "DMSO"].sample(
         n=n_controls, random_state=seed
@@ -362,10 +286,10 @@ wells_heldout_df = pd.concat(wells_heldout_df)
 # take the indices of the held out data frame and use it to drop those samples from
 # the main dataset. And then check if those indices are dropped
 wells_idx_to_drop = wells_heldout_df.index.tolist()
-labeled_df = labeled_df.drop(wells_idx_to_drop)
+fs_profile_df = fs_profile_df.drop(wells_idx_to_drop)
 assert all(
     [
-        True if num not in labeled_df.index.tolist() else False
+        True if num not in fs_profile_df.index.tolist() else False
         for num in treatment_idx_to_drop
     ]
 ), "index to be dropped found in the main dataframe"
@@ -377,14 +301,12 @@ wells_heldout_df.to_csv(
 
 # display
 print("Wells holdout shape:", wells_heldout_df.shape)
-wells_heldout_df.head(10)
+wells_heldout_df.head()
 
 
 # > Table containig all the held out treatment samples. Saved as `wells_holdout.csv.gz` in the `results/0.data_splits`
 
-#
-
-# In[14]:
+# In[12]:
 
 
 # plate
@@ -396,19 +318,19 @@ np.random.seed(seed)
 
 # selecting 10 plates randomly from a list
 selected_plates = (
-    np.random.choice(labeled_df["Plate"].unique().tolist(), (n_plates, 1))
+    np.random.choice(fs_profile_df["Plate"].unique().tolist(), (n_plates, 1))
     .flatten()
     .tolist()
 )
-plate_holdout_df = labeled_df.loc[labeled_df["Plate"].isin(selected_plates)]
+plate_holdout_df = fs_profile_df.loc[fs_profile_df["Plate"].isin(selected_plates)]
 
 # take the indices of the held out data frame and use it to drop those samples from
 # the main dataset. And then check if those indices are dropped
 plate_idx_to_drop = plate_holdout_df.index.tolist()
-labeled_df = labeled_df.drop(plate_idx_to_drop)
+fs_profile_df = fs_profile_df.drop(plate_idx_to_drop)
 assert all(
     [
-        True if num not in labeled_df.index.tolist() else False
+        True if num not in fs_profile_df.index.tolist() else False
         for num in treatment_idx_to_drop
     ]
 ), "index to be dropped found in the main dataframe"
@@ -423,16 +345,12 @@ print("plate holdout shape:", plate_holdout_df.shape)
 plate_holdout_df.head()
 
 
-# ### Training and Test sets
-#
-# Below we are going to use the remaining dataset and split them into test and training sets
-
-# In[15]:
+# In[13]:
 
 
 # Showing the amount of data we have after removing the holdout data
 meta_injury = []
-for injury_type, df in labeled_df.groupby("injury_type"):
+for injury_type, df in fs_profile_df.groupby("injury_type"):
     # extract n_wells, n_compounds and unique compounds per injury_type
     n_wells = df.shape[0]
     unique_compounds = list(df["Compound Name"].unique())
@@ -441,17 +359,31 @@ for injury_type, df in labeled_df.groupby("injury_type"):
     # store information
     meta_injury.append([injury_type, n_wells, n_compounds, unique_compounds])
 
+# creating data frame
 injury_meta_df = pd.DataFrame(
     meta_injury, columns=["injury_type", "n_wells", "n_compounds", "compound_list"]
 ).sort_values("n_wells", ascending=False)
 injury_meta_df.to_csv(data_split_dir / "injury_well_counts_table.csv", index=False)
 
 # display
-print("shape:", injury_meta_df.shape)
 injury_meta_df
 
 
-# In[16]:
+# In[14]:
+
+
+# shape of the update training and testing dataset after removing holdout
+print("training shape after removing holdouts", fs_profile_df.shape)
+fs_profile_df.head()
+
+
+# ### Splitting the data
+#
+# Splitting the data and saving them into csv files:
+# Files are split into test and training dataset.
+#
+
+# In[15]:
 
 
 # spliting the meta features and the feature column names
@@ -464,20 +396,20 @@ y_col = "injury_type"
 X_cols = feature_info["feature_cols"]
 
 
-# In[17]:
+# In[16]:
 
 
 # spliting the dataset
 seed = 0
 
-X = labeled_df[X_cols]
-y = labeled_df[y_col]
+X = fs_profile_df[X_cols]
+y = fs_profile_df[y_col]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.20, random_state=seed
 )
 
 
-# In[18]:
+# In[17]:
 
 
 X_train.to_csv(data_split_dir / "X_train.csv.gz", index=False, compression="gzip")
