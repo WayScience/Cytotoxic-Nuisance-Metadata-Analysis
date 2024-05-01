f800dc91ddca94cd208ba41d6adf71aa57ed66bc
axiomcura <erikishere3@gmail.com>
2024-04-30 20:22:33 -0600

    updated patch-work figure

--- notebooks/4.visualization/Figure2_model_performance_and_eval.r
+++ notebooks/4.visualization/Figure2_model_performance_and_eval.r
@@ -275,7 +275,7 @@ ggsave(filename = "figures/JUMP_cell_injury_facet_proba.png", height = img_heigh
 probabilities_ridge_plot
 
 # plot dims
-height = 17
+height = 20
 width = 25
 
 options(repr.plot.width=width, repr.plot.height=height, units = "cm", dpi = 600)
@@ -297,9 +297,7 @@ fig2 <- (
 ) + plot_annotation(tag_levels = "A") + plot_layout(heights = c(3, 3))
 
 
-# # Adjust layout and appearance (optional)
-# combined_plot <- combined_plot +
-#   plot_layout(ncol = 2, nrow = 2, byrow = TRUE)  # Adjust the grid layout as needed
-
 # Display the combined plot
 fig2
+
+ggsave(plot = fig2, filename = "Figure2_model_performance_and_JUMP.png", height = height, width = width, dpi=600)
